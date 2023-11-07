7a26619dd1a2fdfc425c65377268e5a06c3dd906
Prafull Salunke <prafull.salunke@gupshup.io>
2023-11-07 13:07:30 +0530

    Spring MVC Rest Services : HTTP GET with Spring MVC List Operation

--- src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
+++ src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
@@ -4,16 +4,24 @@ package prafull.springboot.spring6restmvc.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import prafull.springboot.spring6restmvc.model.Beer;
 import prafull.springboot.spring6restmvc.services.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@Controller
+@RestController
 @AllArgsConstructor
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beers")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
     public Beer getBeerById(UUID id){
         log.debug("Get Beer by Id - in controller");
         return beerService.getBeerById(id);
--- src/main/java/prafull/springboot/spring6restmvc/services/BeerService.java
+++ src/main/java/prafull/springboot/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package prafull.springboot.spring6restmvc.services;
 
 import prafull.springboot.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
