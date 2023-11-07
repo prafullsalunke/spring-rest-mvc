186c473e063b7d955702f3f831985b4357f5b4c2
Prafull Salunke <prafull.salunke@gupshup.io>
2023-11-07 13:22:00 +0530

    Spring MVC Rest Services : Using path parameters, Get By Id

--- src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
+++ src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ package prafull.springboot.spring6restmvc.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import prafull.springboot.spring6restmvc.model.Beer;
 import prafull.springboot.spring6restmvc.services.BeerService;
@@ -15,15 +17,18 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
-    public Beer getBeerById(UUID id){
+
+    @RequestMapping(value ="{beerId}", method = RequestMethod.GET)
+    public Beer getBeerByI(@PathVariable("beerId") UUID beerId){
         log.debug("Get Beer by Id - in controller");
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
