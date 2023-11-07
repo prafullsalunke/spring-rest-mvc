0a6aabbeff30082b241add2377ad54630926b3ce
Prafull Salunke <prafull.salunke@gupshup.io>
2023-11-07 14:32:39 +0530

    Spring MVC Rest Services : HTTP POST with Spring MVC

--- src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
+++ src/main/java/prafull/springboot/spring6restmvc/controller/BeerController.java
@@ -3,11 +3,9 @@ package prafull.springboot.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import prafull.springboot.spring6restmvc.model.Beer;
 import prafull.springboot.spring6restmvc.services.BeerService;
 
@@ -21,6 +19,12 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
 
+    @PostMapping
+    public ResponseEntity handlPost(@RequestBody Beer beer){
+        Beer savedBeer = beerService.saveNewBeer(beer);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
@@ -28,7 +32,7 @@ public class BeerController {
 
     @RequestMapping(value ="{beerId}", method = RequestMethod.GET)
     public Beer getBeerByI(@PathVariable("beerId") UUID beerId){
-        log.debug("Get Beer by Id - in controller");
+        log.debug("Get Beer by Id in controller 123 " +beerId);
         return beerService.getBeerById(beerId);
     }
 }
--- src/main/java/prafull/springboot/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/prafull/springboot/spring6restmvc/services/BeerServiceImpl.java
@@ -68,4 +68,21 @@ public class BeerServiceImpl implements BeerService {
         log.debug("Get Beer ID in service was called : "+id);
         return beerMap.get(id);
     }
+
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .upc(beer.getUpc())
+                .version(beer.getVersion())
+                .price(beer.getPrice())
+                .build();
+        beerMap.put(savedBeer.getId(), savedBeer);
+        return savedBeer;
+    }
 }
